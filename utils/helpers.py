919b835ae55b95c125dde0613333b0f2d1c17c99
cakuang1 <70300980+cakuang1@users.noreply.github.com>
2023-05-03 12:37:06 -0700

    new stuff

--- utils/helpers.py
+++ utils/helpers.py
@@ -14,21 +14,6 @@ def timetostring():
 
     return datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
 
-def removecolumns(df,columns):
-   """
-    
-   Removes irrelavent columns for our production table
-
-
-    Arguments : 
-        df (pd.DataFrame): The dataframe you want to drop columns from 
-        columns (list[str]) :  The list of columns you want to drop 
-
-    Returns : 
-        pd.DataFrame 
-   """
-   newdf = df.drop(columns)
-   return newdf
 
 def addminutefield(df):
    """
